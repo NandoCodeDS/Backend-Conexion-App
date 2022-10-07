149527c8ce04b6a67a27e6d7b618048886964aff
NandoCodeDS <99371351+NandoCodeDS@users.noreply.github.com>
2022-10-07 12:39:54 -0500

    Cambios al Demo
    Estos osn cambios de control de versión

--- src/main/java/com/example/demo/persona/rest/LoginREST.java
+++ src/main/java/com/example/demo/persona/rest/LoginREST.java
@@ -11,7 +11,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/usuarios/")
+@RequestMapping("/log/")
 public class LoginREST {
     @Autowired
     private LoginService loginService;
